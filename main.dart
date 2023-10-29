0c3aa056011ee86cb1ae8c903b3370cdfa046cce
settimogiacomo <settimogiacomo.98@gmail.com>
2023-10-29 22:11:54 +0100

    Aggiunta chat con bottoni invia ed elimina, lista con file caricati

--- main.dart
+++ main.dart
@@ -1,9 +1,24 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+class ElencoFileCaricati {
+  List<String> _fileCaricati = [];
+
+  void aggiungiFile(String nomeFile) {
+    _fileCaricati.add(nomeFile);
+  }
+
+  List<String> get elencoCompleto => _fileCaricati;
+
+  int get numeroFileCaricati => _fileCaricati.length;
+}
+
+List<String> _messaggiChat = []; // Dichiarazione della lista _messaggiChat
+
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DocuBot',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -25,21 +41,20 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String? dropdownValue1 = 'Item 1';
-  String? dropdownValue2 = 'Option A';
+  final ElencoFileCaricati _elencoFileCaricati = ElencoFileCaricati(); // Aggiunta dell'oggetto ElencoFileCaricati
 
-  _pickFile() async {
+  void _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
 
     if (result != null) {
-      // Eseguire le operazioni desiderate con il file selezionato
-      print(result.files.single.name);
-      print(result.files.single.path);
+      String nomeFile = result.files.single.name;
+      setState(() {
+        _elencoFileCaricati.aggiungiFile(nomeFile); // Aggiunta del file all'elenco
+      });
     } else {
       // L'utente ha annullato la selezione del file
     }
@@ -58,101 +73,42 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Icon(Icons.flag),
-                      const SizedBox(width: 8),
-                      const Text('Lingua'),
-                      const SizedBox(width: 20),
-                      DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.white, //background color of dropdown button
-                          border: Border.all(color: Colors.black38, width: 2), //border of dropdown button
-                          borderRadius: BorderRadius.circular(50), //border radius of dropdown button
-                        ),
-                        child: Container(
-                          width: 150,
-                          child: DropdownButton<String>(
-                            underline: Container(), // Rimuove la linea sotto il dropdown
-                            value: dropdownValue1,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue1 = newValue;
-                              });
-                            },
-                            isExpanded: true,
-                            style:const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                            ),
-                            items: <String>['Item 1', 'Item 2', 'Item 3', 'Item 4']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Center(
-                                  child: Text(value),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  LabelDropDown(etichetta: "Lingua", lista: 0, valoreDefault: 'Item 1', icona: Icons.flag),
                   const SizedBox(height: 20),
+                  LabelDropDown(etichetta: "Modello", lista: 1, valoreDefault: 'Item 2', icona: Icons.mode_fan_off_rounded),
+                  const SizedBox(height: 80), // Spazio tra il tuo bottone e l'elenco dei file
+
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // Allineamento a sinistra
                     children: [
-                      Icon(Icons.mode_fan_off_rounded),
+                      Icon(Icons.attach_file), // Icona "allega file"
                       const SizedBox(width: 8),
-                      const Text('Modello'),
-                      const SizedBox(width: 20),
-                      DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.white70, //background color of dropdown button
-                          border: Border.all(color: Colors.black38, width: 2), //border of dropdown button
-                          borderRadius: BorderRadius.circular(50), //border radius of dropdown button
-                        ),
-                        child: Container(
-                          width: 150, // Imposta la larghezza desiderata
-                          child: DropdownButton<String>(
-                            underline: Container(), // Rimuove la linea sotto il dropdown
-                            value: dropdownValue2,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue2 = newValue;
-                              });
-                            },
-                            isExpanded: true,
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                            ),
-                            items: <String>['Option A', 'Option B', 'Option C', 'Option D']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Center(
-                                  child: Text(value),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ),
+                      Text('Carica'), // Testo "Carica"
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: _pickFile,
+                        child: const Text('Carica un file'),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 100),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(right: 16.0),
-                          child: ElevatedButton(
-                            onPressed: _pickFile,
-                            child: const Text('Carica un file'),
-                          )
+                  const SizedBox(height: 20), // Spazio tra il tuo bottone e l'elenco dei file
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 50, right: 50, bottom: 50), // Margine esterno di 50 pixel a sinistra, a destra e in basso
+                      padding: const EdgeInsets.all(10), // Padding interno di 10 pixel
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black), // Bordo nero
                       ),
-                    ],
+                      child: ListView.builder(
+                        itemCount: _elencoFileCaricati.numeroFileCaricati, // Usa la lunghezza dell'elenco
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Icon(Icons.insert_drive_file),
+                            title: Text(_elencoFileCaricati.elencoCompleto[index]), // Ottieni il nome del file dall'elenco
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -164,32 +120,192 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Expanded(
             flex: 3,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('Right Part'),
-                  Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  FloatingActionButton(
-                    onPressed: _incrementCounter,
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ),
+            child: SchermataChat(),
           ),
         ],
       ),
     );
   }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class SchermataChat extends StatefulWidget {
+  @override
+  _StatoSchermataChat createState() => _StatoSchermataChat();
+}
+
+class _StatoSchermataChat extends State<SchermataChat> {
+  final TextEditingController _controllerTesto = TextEditingController();
+
+  void _gestisciMessaggioInviato(String messaggio) {
+    if (messaggio.isNotEmpty) {
+      setState(() {
+        _messaggiChat.add(messaggio);
+        _controllerTesto.clear();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.grey,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                height: 620,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Top part with "Chat" text
+                    const Text(
+                      "Chat With Documents",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    // Messaggi della chat
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _messaggiChat.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(_messaggiChat[index]),
+                          );
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 10), // Spazio a sinistra
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Inserisci la tua domanda",
+                                fillColor: Colors.white,
+                                filled: true,
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.green),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10), // Spazio interno
+                              ),
+                              onSubmitted: _gestisciMessaggioInviato,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10), // Spazio tra l'input box e le icone
+                        ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(primary: Colors.green),
+                          onPressed: () {
+                            setState(() {
+                              _gestisciMessaggioInviato(_controllerTesto.text);
+                            });
+                          },
+                          icon: const Icon(Icons.send),
+                          label: const Text("Invia"),
+                        ),
+                        const SizedBox(width: 10), // Spazio tra le icone
+                        ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(primary: Colors.red),
+                          onPressed: () {
+                            setState(() {
+                              _messaggiChat.clear(); // Cancella tutti i messaggi
+                            });
+                          },
+                          icon: const Icon(Icons.delete),
+                          label: const Text("Svuota Chat"),
+                        ),
+                        const SizedBox(width: 10), // Spazio tra le icone
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LabelDropDown extends StatefulWidget {
+  final String etichetta;
+  final int lista;
+  String valoreDefault;
+  final IconData icona;
+
+  LabelDropDown({
+    required this.etichetta,
+    required this.lista,
+    required this.valoreDefault,
+    required this.icona,
+  });
+
+  @override
+  _LabelDropDownState createState() => _LabelDropDownState();
+}
+
+class _LabelDropDownState extends State<LabelDropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Row(
+          children: <Widget>[
+            Icon(widget.icona),
+            const SizedBox(width: 8),
+            Text(widget.etichetta),
+          ],
+        ),
+        const SizedBox(width: 5),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black38, width: 2),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5), // Aggiungi spazio intorno alle Dropdown
+            child: Container(
+              width: 150,
+              child: DropdownButton<String>(
+                underline: Container(),
+                value: widget.valoreDefault,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    widget.valoreDefault = newValue!;
+                  });
+                },
+                isExpanded: true,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+                items: <String>['Item 1', 'Item 2', 'Item 3', 'Item 4']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Center(
+                      child: Text(value),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
