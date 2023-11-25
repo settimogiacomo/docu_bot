6bd20c20992e79a4ccde7569c466346cc07561e2
settimogiacomo <settimogiacomo.98@gmail.com>
2023-11-25 22:53:06 +0100

    Interrogazione server

--- main.dart
+++ main.dart
@@ -1,6 +1,11 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
 class ElencoFileCaricati {
   List<String> _fileCaricati = [];
 
@@ -13,13 +18,12 @@ class ElencoFileCaricati {
   int get numeroFileCaricati => _fileCaricati.length;
 }
 
-List<String> _messaggiChat = []; // Dichiarazione della lista _messaggiChat
+List<String> _messaggiChat = [];
 const double FONTSIZE = 13.5;
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -46,7 +50,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final ElencoFileCaricati _elencoFileCaricati = ElencoFileCaricati(); // Aggiunta dell'oggetto ElencoFileCaricati
+  final ElencoFileCaricati _elencoFileCaricati = ElencoFileCaricati();
 
   void _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
@@ -54,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
     if (result != null) {
       String nomeFile = result.files.single.name;
       setState(() {
-        _elencoFileCaricati.aggiungiFile(nomeFile); // Aggiunta del file all'elenco
+        _elencoFileCaricati.aggiungiFile(nomeFile);
       });
     } else {
       // L'utente ha annullato la selezione del file
@@ -77,14 +81,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   LabelDropDown(etichetta: "Lingua", lista: <String>["Italiano", "Inglese", "Spagnolo", "Tedesco"], valoreDefault: 'Italiano', icona: Icons.flag),
                   const SizedBox(height: 20),
                   LabelDropDown(etichetta: "Modello", lista: <String>["google/flan-t5-xxl", "google/flan-t5-large", "tiiuae/falcon-7b-instruct"], valoreDefault: 'google/flan-t5-xxl', icona: Icons.mode_fan_off_rounded),
-                  const SizedBox(height: 80), // Spazio tra il tuo bottone e l'elenco dei file
+                  const SizedBox(height: 80),
 
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center, // Allineamento a sinistra
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.attach_file), // Icona "allega file"
+                      Icon(Icons.attach_file),
                       const SizedBox(width: 8),
-                      Text('Carica', style: TextStyle(fontSize: FONTSIZE) ), // Testo "Carica"
+                      Text('Carica', style: TextStyle(fontSize: FONTSIZE)),
                       const SizedBox(width: 8),
                       ElevatedButton(
                         onPressed: _pickFile,
@@ -92,21 +96,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20), // Spazio tra il tuo bottone e l'elenco dei file
+                  const SizedBox(height: 20),
                   Expanded(
                     child: Container(
-                      margin: const EdgeInsets.only(left: 50, right: 50, bottom: 50), // Margine esterno di 50 pixel a sinistra, a destra e in basso
-                      padding: const EdgeInsets.all(10), // Padding interno di 10 pixel
+                      margin: const EdgeInsets.only(left: 50, right: 50, bottom: 50),
+                      padding: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black38), // Bordo nero
-                        borderRadius: BorderRadius.circular(15)
+                        border: Border.all(color: Colors.black38),
+                        borderRadius: BorderRadius.circular(15),
                       ),
                       child: ListView.builder(
-                        itemCount: _elencoFileCaricati.numeroFileCaricati, // Usa la lunghezza dell'elenco
+                        itemCount: _elencoFileCaricati.numeroFileCaricati,
                         itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: Icon(Icons.insert_drive_file),
-                            title: Text(_elencoFileCaricati.elencoCompleto[index], style: TextStyle(fontSize: FONTSIZE)), // Ottieni il nome del file dall'elenco
+                          return Align(
+                            alignment: Alignment.topRight,
+                            child: ListTile(
+                              title: Text(_elencoFileCaricati.elencoCompleto[index], style: TextStyle(fontSize: FONTSIZE)),
+                            ),
                           );
                         },
                       ),
@@ -135,23 +141,89 @@ class SchermataChat extends StatefulWidget {
   _StatoSchermataChat createState() => _StatoSchermataChat();
 }
 
+const IconData userIcon = Icons.account_circle_rounded;
+const IconData responseIcon = Icons.adb;
+
 class _StatoSchermataChat extends State<SchermataChat> {
   final TextEditingController _controllerTesto = TextEditingController(text: "");
 
-  @override
-  void dispose() {
-    _controllerTesto.dispose();
-    super.dispose();
-  }
-
   void _gestisciMessaggioInviato(String messaggio) {
     if (messaggio.isNotEmpty) {
-      setState(() {
-        _messaggiChat.add(messaggio);
-        _controllerTesto.clear();
-      });
+      //Invia messaggio all'Api
+      _inviaEMostraRisposta(messaggio);
     }
   }
+
+  Future<void> _inviaEMostraRisposta(String messaggio) async {
+    // Invia messaggio all'API
+    final risposta  = await _richiediRispostaDallApi(messaggio);
+
+    // Aggiorna la chat con il messaggio inviato
+    setState(() {
+      _messaggiChat.add(messaggio);
+      _controllerTesto.clear();
+    });
+    setState(() {
+      _messaggiChat.add(risposta);
+      _controllerTesto.clear();
+    });
+    // Attendi la risposta dall'API e aggiorna la chat con la risposta
+    //final risposta = await _richiediRispostaDallApi();
+    //setState(() {
+    //  _messaggiChat.add(risposta);
+    //});
+  }
+
+  Future<void> _inviaMessaggioAllApi(String messaggio) async {
+    var url = 'http://localhost:8080/question'; // Sostituisci con l'URL del tuo server
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json', // Specifica il tipo di contenuto come JSON
+        },
+        body: jsonEncode({
+          'messaggio': messaggio,
+        }),
+      );
+
+      if (response.statusCode != 200) {
+        print('Errore durante l\'invio del messaggio. Codice: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Errore durante la richiesta HTTP: $e');
+    }
+  }
+
+
+  Future<String> _richiediRispostaDallApi(String domanda) async {
+    if (Uri == null){
+      return 'Uri null';
+    }
+    //try {
+      var url = Uri.parse('http://localhost:8080/question/$domanda'); // Sostituisci con l'URL del tuo server
+      print(url);
+      var response = await http.get(url, headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+      }); //, headers: {'Content-Type': 'application/json'});
+
+      if (response.statusCode == 200) {
+        var decodedResponse = json.decode(response.body);
+        // Assume che la risposta JSON contenga un campo specifico, ad esempio "risposta"
+        final risposta = decodedResponse['question'];
+
+        return 'Risposta ricevuta: $risposta';
+      } else {
+        return 'Errore durante la richiesta di risposta. Codice: ${response.statusCode}';
+      }
+    //} catch (e) {
+    //  return 'Errore durante la richiesta HTTP: $e';
+    //}
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -175,7 +247,6 @@ class _StatoSchermataChat extends State<SchermataChat> {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    // Top part with "Chat" text
                     const Padding(
                       padding: EdgeInsets.only(top:10),
                       child: Text(
@@ -184,13 +255,27 @@ class _StatoSchermataChat extends State<SchermataChat> {
                         style: TextStyle(fontSize: FONTSIZE+5.0, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    // Messaggi della chat
                     Expanded(
                       child: ListView.builder(
                         itemCount: _messaggiChat.length,
                         itemBuilder: (context, index) {
+                          final isMessaggioUtente = index % 2 == 0;
                           return ListTile(
-                            title: Text(_messaggiChat[index], style: TextStyle(fontSize: FONTSIZE)),
+                            title: Align(
+                              alignment: isMessaggioUtente ? Alignment.topRight : Alignment.topLeft,
+                              child: Row(
+                                mainAxisAlignment: isMessaggioUtente ? MainAxisAlignment.end : MainAxisAlignment.start,
+                                children: [
+                                  // Usa l'icona dell'utente per il messaggio dell'utente
+                                  Icon(isMessaggioUtente ? userIcon : responseIcon),
+                                  SizedBox(width: 8), // Adjust the spacing between icon and text
+                                  Text(
+                                    _messaggiChat[index],
+                                    style: TextStyle(fontSize: FONTSIZE),
+                                  ),
+                                ],
+                              ),
+                            ),
                           );
                         },
                       ),
@@ -203,7 +288,7 @@ class _StatoSchermataChat extends State<SchermataChat> {
                           Expanded(
                             flex: 2,
                             child: Container(
-                              margin: const EdgeInsets.only(left: 10), // Spazio a sinistra
+                              margin: const EdgeInsets.only(left: 10),
                               child: TextField(
                                 controller: _controllerTesto,
                                 decoration: InputDecoration(
@@ -214,36 +299,36 @@ class _StatoSchermataChat extends State<SchermataChat> {
                                     borderSide: BorderSide(color: Colors.green),
                                     borderRadius: BorderRadius.circular(15),
                                   ),
-                                  contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10), // Spazio interno
+                                  contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
                                 ),
                                 onSubmitted: _gestisciMessaggioInviato,
                               ),
                             ),
                           ),
-                          const SizedBox(width: 10), // Spazio tra l'input box e le icone
+                          const SizedBox(width: 10),
                           ElevatedButton.icon(
                             style: ElevatedButton.styleFrom(primary: Colors.green),
                             onPressed: () {
                               setState(() {
-                                _gestisciMessaggioInviato(_controllerTesto.text);
+                                _inviaEMostraRisposta(_controllerTesto.text);
                               });
                             },
                             icon: const Icon(Icons.send),
                             label: const Text("Invia"),
                           ),
-                          const SizedBox(width: 10), // Spazio tra le icone
+                          const SizedBox(width: 10),
                           ElevatedButton.icon(
                             style: ElevatedButton.styleFrom(primary: Colors.red),
                             onPressed: () {
                               setState(() {
-                                _messaggiChat.clear(); // Cancella tutti i messaggi
+                                _messaggiChat.clear();
                                 _controllerTesto.clear();
                               });
                             },
                             icon: const Icon(Icons.delete),
                             label: const Text("Svuota Chat"),
                           ),
-                          const SizedBox(width: 10), // Spazio tra le icone
+                          const SizedBox(width: 10),
                         ],
                       ),
                     ),
@@ -295,7 +380,7 @@ class _LabelDropDownState extends State<LabelDropDown> {
             borderRadius: BorderRadius.circular(15),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(0.5), // Aggiungi spazio intorno alle Dropdown
+            padding: const EdgeInsets.all(0.5),
             child: Container(
               width: 230,
               child: DropdownButton<String>(
