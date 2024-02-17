d92cc619a8fbac4c44b117dd7fc3c8938b1bb9a7
Sara <saradegra@gmail.com>
2024-02-17 22:53:43 +0100

    Aggiunto logo per studenti

--- MyHomePage.dart
+++ MyHomePage.dart
@@ -1,25 +1,27 @@
 import 'package:docu_bot/Requests.dart';
+import 'package:docu_bot/ElencoFileCaricati.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-
 import 'costanti.dart';
 import 'LabelDropDown.dart';
 import 'SchermataChat.dart';
 import 'ElencoFileCaricati.dart';
 
-
 var _lingua = 'Italiano';
 var _nomeModello = 'google/flan-t5-xxl';
 
 class MyHomePage extends StatefulWidget {
-  final String username = '';
-  const MyHomePage({Key? key, required this.title, required username}) : super(key: key);
+  final String username;
+  const MyHomePage({Key? key, required this.title, required this.username})
+      : super(key: key);
   final String title;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(nomeUtente: username);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String nomeUtente;
+  _MyHomePageState({Key? key, required this.nomeUtente});
 
   var _hasContent = false;
   var _fileSelezionato = -1;
@@ -28,10 +30,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    contaFilesGET().then((value) => _aggiornaElencoFiles(value));
+    print(nomeUtente);
+    print(nomeUtente.toLowerCase() == "studente");
+    contaFilesGET().then((value) => aggiornaElencoFiles(value));
   }
 
-  void _aggiornaElencoFiles(files){
+  void aggiornaElencoFiles(files) {
     setState(() {
       _elencoFileCaricati = ElencoFileCaricati.withFiles(files);
       _hasContent = (_elencoFileCaricati.contaFileCaricati > 0);
@@ -44,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     if (resultFile != null) {
       bool aggiorna = await _elencoFileCaricati.salvaFile(resultFile);
-      if(aggiorna){
+      if (aggiorna) {
         setState(() {
           _hasContent = (_elencoFileCaricati.contaFileCaricati > 0);
         });
@@ -59,86 +63,126 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Row(
         children: [
-          Expanded(
-            flex: 1,
+          Visibility(
+            visible: (nomeUtente.toLowerCase() == "studente"),
             child: Container(
-              color: Colors.white70,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(height: 20),
-                    Row(
+              padding: EdgeInsets.all(20), // Padding di 20 su tutti i lati
+              color: Colors.white, // Sfondo bianco
+              child: Expanded(
+                flex: 1,
+                child: Image.asset('lib/assets/logo.png'),
+                //child:Image.network('https://upload.wikimedia.org/wikipedia/commons/7/7c/Logo_UniTO_2022.svg')
+              ),
+            ),
+          ),
+          Visibility(
+            visible: (nomeUtente.toLowerCase() != "studente"),
+            child: Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white70,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(height: 20),
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          LabelDropDown(etichetta: "Lingua", lista: <String>["Italiano", "Inglese", "Spagnolo", "Tedesco"], valoreDefault: _lingua, icona: Icons.flag),
-                        ]
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        LabelDropDown(etichetta: "Modello", lista: <String>["google/flan-t5-xxl", "google/flan-t5-large", "tiiuae/falcon-7b-instruct"], valoreDefault: _nomeModello, icona: Icons.mode_fan_off_rounded),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(Icons.attach_file),
-                        const SizedBox(width: 8),
-                        Text('Carica', style: TextStyle(fontSize: FONTSIZE)),
-                        const SizedBox(width: 8),
-                        Container(
-                          height: 45.0,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.lightBlueAccent,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(BORDER_RADIUS),
-                              ),
-                            ),
-                            onPressed: _pickFile,
-                            child: const Text('Carica un file'),
+                          LabelDropDown(
+                            etichetta: "Lingua",
+                            lista: <String>[
+                              "Italiano",
+                              "Inglese",
+                              "Spagnolo",
+                              "Tedesco"
+                            ],
+                            valoreDefault: _lingua,
+                            icona: Icons.flag,
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Visibility(
-                      visible: _hasContent,
-                      child: Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black38),
-                            borderRadius: BorderRadius.circular(BORDER_RADIUS),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          LabelDropDown(
+                            etichetta: "Modello",
+                            lista: <String>[
+                              "google/flan-t5-xxl",
+                              "google/flan-t5-large",
+                              "tiiuae/falcon-7b-instruct"
+                            ],
+                            valoreDefault: _nomeModello,
+                            icona: Icons.mode_fan_off_rounded,
                           ),
-                          child: ListView.builder(
-                            itemCount: _elencoFileCaricati.contaFileCaricati,
-                            itemBuilder: (context, index) {
-                              return Align(
-                                alignment: Alignment.topRight,
-                                child: ListTile(
-                                  title: Text(_elencoFileCaricati.elencoCompleto[index],
-                                      style: TextStyle(fontSize: FONTSIZE)
-                                  ),
-                                  tileColor: _fileSelezionato == index ? Colors.blue : null,
-                                  onTap: () {
-                                    setState(() {
-                                      _fileSelezionato = index;
-                                    });
-                                  },
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.attach_file),
+                          const SizedBox(width: 8),
+                          Text('Carica', style: TextStyle(fontSize: FONTSIZE)),
+                          const SizedBox(width: 8),
+                          Container(
+                            height: 45.0,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.lightBlueAccent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                  BorderRadius.circular(BORDER_RADIUS),
                                 ),
-                              );
-                            },
+                              ),
+                              onPressed: _pickFile,
+                              child: const Text('Carica un file'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Visibility(
+                        visible: _hasContent,
+                        child: Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                                left: 10, right: 10, bottom: 10),
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black38),
+                              borderRadius:
+                              BorderRadius.circular(BORDER_RADIUS),
+                            ),
+                            child: ListView.builder(
+                              itemCount: _elencoFileCaricati.contaFileCaricati,
+                              itemBuilder: (context, index) {
+                                return Align(
+                                  alignment: Alignment.topRight,
+                                  child: ListTile(
+                                    title: Text(
+                                        _elencoFileCaricati
+                                            .elencoCompleto[index],
+                                        style: TextStyle(fontSize: FONTSIZE)),
+                                    tileColor: _fileSelezionato == index
+                                        ? Colors.blue
+                                        : null,
+                                    onTap: () {
+                                      setState(() {
+                                        _fileSelezionato = index;
+                                      });
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@ dev_dependencies:
     flutter_test:
     sdk: flutter
     http: ^1.1.0
-    speech_to_text: ^5.5.0
-    permission_handler: ^9.2.0
-    flutter_tts: ^3.8.5
+    speech_to_text: 5.5.0
+    speech_to_text_platform_interface: 2.1.0
+    permission_handler: 9.2.0
+    flutter_tts: 3.8.5
